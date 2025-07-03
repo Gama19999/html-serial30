f4e7d1b4afb2d7223d5e4ba79160b2aa792a445d
Gama19999 <gama.rios0110@gmail.com>
2025-07-03 00:38:13 -0600

    Version 1.3.2
    Refactored hard strings
    Fixed privacy policy mobile view

--- src/environments/environment.web.ts
+++ src/environments/environment.web.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appName: 'serial30',
-  version: '1.3.1',
+  version: '1.3.2',
   api: 'https://serial30.ovh/api/',
   email: 'mailto:gama.rios0110@gmail.com?subject=Message%20sent%20from%20SERIAL30%20web',
   whatsapp: 'https://wa.me/message/WSPGNKDWJ5BKC1',
