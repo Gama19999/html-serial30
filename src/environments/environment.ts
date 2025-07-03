cd0a850d8cc999eb559514bf02a10b5fbdf571ed
Gama19999 <gama.rios0110@gmail.com>
2025-07-03 00:10:46 -0600

    Version 1.3.1
    Added environment for web

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   appName: 'serial30',
-  version: '1.3.0',
+  version: '1.3.1',
   api: 'http://localhost:8031/api/',
   email: 'mailto:gama.rios0110@gmail.com?subject=Message%20sent%20from%20SERIAL30%20web',
   whatsapp: 'https://wa.me/message/WSPGNKDWJ5BKC1',
